0643720780f3ec0302f5b68872a083951dfe91c2
Sarna <olsarnat@gmail.com>
2019-12-21 00:36:43 +0200

    2th commit try someone

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootwatch/journal/bootstrap.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <WetherDisplay zip=("1,2,3,4,5")/>
     </div>
   );
 }
